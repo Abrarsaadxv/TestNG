680a2aefd1bff4ef9070791821cf53c787ed7ca9
Abrarsaad <abrarsaadd1@gmail.com>
2024-03-20 11:41:29 +0300

    HW3

---
+++ src/test/java/SDA/test/day19/C03AllureTest01.java
@@ -0,0 +1,39 @@
+package SDA.test.day19;
+
+    import SDA.utilities.TestBase;
+    import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+public class C03AllureTest01  extends TestBase {
+
+        @Test
+        public void negativeUserNameTestSoft() throws InterruptedException {
+            //Open page https://practicetestautomation.com/practice-test-login/
+            driver.get("https://practicetestautomation.com/practice-test-login/");
+            //Type username student into Username field
+            driver.findElement(By.xpath("//input[@name='username']")).sendKeys("incorrectUser");
+            //Type password Password123 into Password field
+            driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Password123");
+            driver.findElement(By.cssSelector("button#submit")).click();//Click Submit button.
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement message = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("error"))));
+
+            SoftAssert sa = new SoftAssert();
+
+            //Verify error message is displayed.
+            sa.assertTrue(message.isDisplayed());
+
+            //Verify error message text is Your username is invalid!
+            sa.assertEquals(message.getText(), "Your username is invalid!");
+
+            sa.assertAll();
+        }
+
+
+    }
